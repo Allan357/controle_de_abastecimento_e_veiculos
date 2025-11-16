3a658a2385fddaa75074db60681b0940f093caa0
Allan de Alencar <183562445+Allan357@users.noreply.github.com>
2025-11-16 00:53:14 -0400

    setup: estrutura inicial do projeto

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:controle_de_abastecimento_e_veiculos/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    
+    await dotenv.load(fileName: "../.env");
+
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+    
+    runApp(App());
+    }
+
+    class App extends StatelessWidget {
+    const App({super.key});
+    
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+        home: Scaffold(
+            body: Text("Estrutura inicial do projeto!"),
+        ));
+    }
+}
